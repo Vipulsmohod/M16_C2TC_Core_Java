c65d73f3a7c17d6397c5051a34394d0af8870f03
shree <shree@LAPTOP-SAQM0JOD>
2022-04-09 23:53:04 +0530

    first commit

---
+++ Day2_Introduction_Of_Java/src/com/cg/model/Example1.java
@@ -0,0 +1,22 @@
+package com.cg.model;
+
+public class Example1 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		float a1=10.25f;   
+		double a2=45.12d;
+		char c='x';
+		String s="Vipul Mohod";
+		
+
+		System.out.println(a);
+		System.out.println(a2);
+		System.out.println(a1);
+		System.out.println(s);
+		System.out.println(c);
+		
+	}
+
+}
---
+++ Day2_Introduction_Of_Java/src/com/cg/model/Example2.java
@@ -0,0 +1,15 @@
+package com.cg.model;
+
+import java.util.Scanner;
+
+public class Example2 
+{
+	public static void main(String[] args) {
+		  Scanner sc=new Scanner(System.in);
+		  System.out.println("Enter The Any Integer Value");
+		  int a=sc.nextInt();
+		  System.out.println("Your Value is ="+a);
+		  sc.close();
+	}
+
+}
