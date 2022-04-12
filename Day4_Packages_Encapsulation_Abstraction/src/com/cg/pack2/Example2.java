99ee2206d7c62544df818ac3e676aadc77d0097d
shree <shree@LAPTOP-SAQM0JOD.bbrouter>
2022-04-12 15:34:29 +0530

    first commit

---
+++ Day4_Packages_Encapsulation_Abstraction/src/com/cg/pack2/Example2.java
@@ -0,0 +1,21 @@
+package com.cg.pack2;
+
+import com.cg.pack1.Example1;
+
+public class Example2 
+{
+	void m1()
+	{
+		System.out.println("m1----Of----Example 2");
+	}
+	public static void main(String[] args) {
+		
+		Example1 e=new Example1();
+		 e.m1();
+		 Example2 e1=new Example2();
+		 e1.m1();
+	}
+
+	
+	
+}
