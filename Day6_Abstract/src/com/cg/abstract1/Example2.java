b4971947698e1931df2ea5aecf32e6954dd3e569
shree <shree@LAPTOP-SAQM0JOD>
2022-04-17 22:58:30 +0530

    first commit

---
+++ Day6_Abstract/src/com/cg/abstract1/Example2.java
@@ -0,0 +1,15 @@
+package com.cg.abstract1;
+
+public class Example2 {
+	
+	
+
+	public static void main(String[] args) 
+	{
+		final int a=50;
+		//a=14;
+		System.out.println(a);
+
+	}
+
+}
---
+++ Day6_Abstract/src/com/cg/abstract1/Example3.java
@@ -0,0 +1,24 @@
+package com.cg.abstract1;
+class Parent
+{
+	final void accept()
+	{
+		System.out.print("Parent");
+	}
+}
+class Child extends Parent
+{
+	void accept1()
+	{
+		System.out.print("Child");
+	}
+}
+public class Example3 {
+
+	public static void main(String[] args) {
+		Child c=new Child();
+		c.accept1();
+
+	}
+
+}
