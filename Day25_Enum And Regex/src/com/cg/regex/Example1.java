2fc1b8564f037ba8e0ba1940c1f673b7fae68476
shree <shree@192.168.1.7>
2022-05-23 13:43:29 +0530

    First Commit

---
+++ Day25_Enum And Regex/src/com/cg/regex/Example1.java
@@ -0,0 +1,24 @@
+package com.cg.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Example1 
+{
+
+
+	public static void main(String[] args) {
+		String pattern="Vipul Mohod";
+		Scanner s=new Scanner(System.in);
+		String input=s.nextLine();
+		boolean result=Pattern.matches(pattern, input);
+		System.out.println(result);
+		s.close();
+		
+		
+
+	}
+
+}
+
+
---
+++ Day25_Enum And Regex/src/com/cg/regex/Example3.java
@@ -0,0 +1,19 @@
+package com.cg.regex;
+
+import java.util.Scanner;
+public class Example3
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str="M1-16 @@ Pratik Jain @@ Ankita @@ Hrithik";
+		String[] res=str.split("@@");
+		for(String i:res)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+
+
+}
